831104f63d8aafabc208c6a7df37287024b1e75a
mahmoud <76081182+mahmoude4477@users.noreply.github.com>
2021-05-02 23:22:16 +0300

    Update telegramA.py

--- telegramA.py
+++ telegramA.py
@@ -7,12 +7,12 @@ def start(update,context):
     user = update.message.chat_id
     try:filee = open('list_Telegram_Id.txt').readlines()
     except:filee= [None]
-    if str(user)+'\n' not in filee:#str(user['id'])+'\n' not in filee:
+    if str(user)+'\n' not in filee:
             with open('list_Telegram_Id.txt','a')as listTele:
                 listTele.write(str(update.message.chat_id)+'\n')
             update.message.reply_text('مرحبا تم اضافتك وسيتم ارسال الاذكار قريبا')
     else:
         update.message.reply_text('مرحبا تم اضافتك مسبقا')
-update = Updater('1630827457:AAHxyYsnRfUIM7XMcU3cvV8_EkE6pB2mWi8')
+update = Updater(token)
 update.dispatcher.add_handler(CommandHandler('startA',start))
 update.start_polling()
